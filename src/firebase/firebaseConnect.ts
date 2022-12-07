00bfeef2fa21848f601bf8df3b535cd0d7147e1c
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-12-07 18:43:20 -0500

    modified firebase file

--- src/firebase/firebaseConnect.ts
+++ src/firebase/firebaseConnect.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -15,5 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+
 
